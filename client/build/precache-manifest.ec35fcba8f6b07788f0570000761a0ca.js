5ad30f914be54aaf92ec2c588e613c3f4a9c6c12
Shaun Kiszonas <smkiszonas@gmail.com>
2022-07-21 17:47:36 -0700

    please work

---
+++ client/build/precache-manifest.ec35fcba8f6b07788f0570000761a0ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b633fdcf74a14c7b445b5587891e911",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0d9f7a590d4181b6e2c",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "76c2b6d6988a378c37b7",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "f0d9f7a590d4181b6e2c",
+    "url": "/static/js/2.a1cbaefd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a1cbaefd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c2b6d6988a378c37b7",
+    "url": "/static/js/main.2aec3983.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
